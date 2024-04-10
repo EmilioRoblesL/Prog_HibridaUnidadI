810f773f915b78170b1534f6f102ec8e67802c94
EmilioRoblesL <ejroblesl24@gmail.com>
2024-04-10 10:51:27 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ph.ppt.u1',
+  appName: 'ph-ppt-u1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
